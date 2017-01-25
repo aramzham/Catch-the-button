b5acc14e249ad3de05c8ffee8a6268eaf19039ae
aramzham <aramzham@hotmail.com>
2017-01-25 17:20:20 +0400

    initial commit not ended!

---
+++ ExperimnetsOnForms/CatchMeIfYouCan/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CatchMeIfYouCan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(665, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(665, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(437, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(437, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(117, 26);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mouse X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(583, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mouse Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Button X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(356, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Button Y";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(538, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 488);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Catch me if you can (feat. Leonardo Di Caprio)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ExperimnetsOnForms/CatchMeIfYouCan/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchMeIfYouCan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.Text = this.PointToClient(Cursor.Position).X.ToString();
+            textBox2.Text = this.PointToClient(Cursor.Position).Y.ToString();
+            textBox3.Text = button1.Location.X.ToString();
+            textBox4.Text = button1.Location.Y.ToString();
+            var delta = 25;
+
+            button1.Location = this.PointToClient(Cursor.Position).X > button1.Location.X ?
+                                                  new Point(button1.Location.X - delta, button1.Location.Y) :
+                                                  new Point(button1.Location.X + delta, button1.Location.Y);
+            button1.Location = this.PointToClient(Cursor.Position).Y > button1.Location.Y ?
+                                                  new Point(button1.Location.X, button1.Location.Y - delta) :
+                                                  new Point(button1.Location.X, button1.Location.Y + delta);
+
+            if (button1.Location.X < 0) button1.Location = new Point(ClientRectangle.Width, button1.Location.Y);
+            //if (button1.Location.X > this.Left) button1.Location = new Point(this.Width, button1.Location.Y);
+            if (button1.Location.Y < 0) button1.Location = new Point(button1.Location.X, ClientRectangle.Bottom);
+            //if (button1.Location.Y > this.Bottom) button1.Location = new Point(button1.Location.X, this.Top);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You caught me, punk!!!");
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (string.Equals((sender as Button)?.Name, @"CloseButton")) MessageBox.Show("You caught me, punk!!!");
+            //// Do something proper to CloseButton.
+            //else 
+            switch (e.CloseReason)
+            {
+                case CloseReason.TaskManagerClosing:
+                    MessageBox.Show("Taks manager?! You're too smart, you, little scum!");
+                    break;
+                case CloseReason.WindowsShutDown:
+                    MessageBox.Show("Only by shutting windows down :-)");
+                    break;
+                case CloseReason.UserClosing:
+                    MessageBox.Show("Don't push that button, nooooooooooooo");
+                    break;
+            }
+            // Then assume that X has been clicked and act accordingly.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = MousePosition.X.ToString();
+            textBox2.Text = MousePosition.Y.ToString();
+            textBox3.Text = button1.Location.X.ToString();
+            textBox4.Text = button1.Location.Y.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Location = new Point(this.ClientSize.Width / 2 - button1.Size.Width / 2, this.ClientSize.Height / 2 - button1.Size.Height / 2);
+        }
+    }
+}
+
